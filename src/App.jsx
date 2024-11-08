b61244b15f7708b5a02c763944265af79a6188b7
sajidhossain8272 <sajidhossain8272@gmail.com>
2024-11-08 13:22:47 +0600

    website under construction

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className='flex justify-center'>
+        <img width='200' src='./Logo-t-2.png' alt='' />
+      </div>
+      <div className='flex justify-center'>
+        <h1 className='text-5xl'>Airdrop Infinity</h1>
+      </div>{" "}
+      <br />
+      <h2 className=' flex justify-center text-3xl'>
+        Website is under construction! Stay Tuned.
+      </h2>
+    </>
+  );
+}
+
+export default App;
