723614c24afbc761f4fd45bd19e2ce2f28083701
sajidhossain8272 <sajidhossain8272@gmail.com>
2024-11-08 19:34:11 +0600

    cosmetic changes

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -2,7 +2,7 @@
 const Banner = () => {
   return (
     <div>
-      <h3 className="lg:pl-96 lg:pt-16 lg:text-6xl text-3xl lg:pr-40 p-4 pl-8">Airdrop Infinity – Bringing you the best crypto airdrops, in 2025!</h3>
+      <h3 className="lg:pl-96 lg:pt-10 lg:text-6xl text-3xl lg:pr-40 p-4 pl-8 lato-bold">Airdrop Infinity – Bringing you the best crypto airdrops, in 2025!</h3>
     </div>
   )
 }
