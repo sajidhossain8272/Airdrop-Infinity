1ae37335632b95c1abdcff024145f875016653d6
sajidhossain8272 <sajidhossain8272@gmail.com>
2024-11-10 13:27:45 +0600

    tomato added

--- src/FeaturedData.js
+++ src/FeaturedData.js
@@ -30,7 +30,34 @@ const FeaturedData = [
     network_logo: "https://cryptologos.cc/logos/solana-sol-logo.png?v=035",
     task: "Running Desktop Node(2x earning), Network mining, Referral",
   },
- 
+ //Tomato
+ {
+  featured_id: "Tomato_Tomarket_Token_Generation_Event_October_Airdrop",
+  featured_title: "Tomarket - TOMA(tge event)",
+  featured_image:
+    "https://firebasestorage.googleapis.com/v0/b/sajid-hossain.appspot.com/o/Airdrop%20Infinity%2FBanner%2FTomato.webp?alt=media&token=41b24c02-dcf6-400e-9a1f-9efd4822f2a8",
+    company_logo:"https://firebasestorage.googleapis.com/v0/b/sajid-hossain.appspot.com/o/Airdrop%20Infinity%2FCompany%20Logo%2Ftomato-logo.webp?alt=media&token=65fe6573-2045-448a-a11c-a64059041b91",
+  category: "telegram",
+  description:
+    "Tomarket is your all-in-one platform for gaming, earning, and trading on Telegram & TON.No more jumping between different platforms—play games, earn tokens, and trade them all in one place.",
+    type:"$TOMA🍅",
+    type2:"Telegram Mini App💎",
+    steps: [
+      "Open the Tomarket Telegram bot to get started: ☞ ☞ https://t.me/Tomarket_ai_bot/app?startapp=0000hpdo.",
+      "Play games within the bot to earn and harvest tomatoes.",
+      "Collect as many tomatoes as possible to increase your rewards.",
+      "Share your unique referral link with friends to earn extra points and tomatoes.",
+      "Follow @rairdropinfiniti for regular updates and upcoming opportunities. Follow here: @airdropinfiniti ☞ https://x.com/@airdropinfiniti",
+      "Visit Tomarket's official website to explore their social channels and stay updated ☞☞ https://tomarket.ai/"
+  ],
+  availability: true,
+  price: 50,
+  listed_price: "N/A (Listing on November)",
+  network: "Ton ",
+  network_logo: "https://cryptologos.cc/logos/solana-sol-logo.png?v=035",
+  task: "Telegram Mini App, Tap Game, Referral",
+},
+
     ];
 
 export default FeaturedData;
