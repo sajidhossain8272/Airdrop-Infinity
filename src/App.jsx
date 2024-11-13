2f9d438d6cd2698b8d7bb87ecbca6e7f569f6c7b
sajidhossain8272 <sajidhossain8272@gmail.com>
2024-11-13 12:47:57 +0600

    test

--- src/App.jsx
+++ src/App.jsx
@@ -12,19 +12,20 @@ function App() {
         <title>
           Airdrop Infinity | Bringing you the best crypto airdrops, in 2025!
         </title>
-        <script type='application/ld+json'>
+        <script type="application/ld+json">
           {`
         {
-         "@context": "https://schema.org",
-  "@type": "Organization",
-  "name": "Airdrop Infinity",
-  "url": "https://www.airdropinfinity.com/",
-  "logo": "https://firebasestorage.googleapis.com/v0/b/sajid-hossain.appspot.com/o/Airdrop%20Infinity%2FLogo%2FFavicon.ico?alt=media&token=37c15381-b8d9-4fa3-adc6-90be2b5f14f4",
-  "description": "Airdrop Infinity – Bringing you the best crypto airdrops, in 2025! Discover and claim top cryptocurrency airdrops from various blockchain projects.",
-  "sameAs": [
-    "https://x.com/airdropinfiniti",
-    "https://www.facebook.com/airdropinfinity",
-    "https://medium.com/@airdropinfinity"
+          "@context": "https://schema.org",
+          "@type": "Organization",
+          "name": "Airdrop Infinity",
+          "url": "https://www.airdropinfinity.com/",
+          "logo": "https://firebasestorage.googleapis.com/v0/b/sajid-hossain.appspot.com/o/Airdrop%20Infinity%2FLogo%2FFavicon.ico?alt=media&token=37c15381-b8d9-4fa3-adc6-90be2b5f14f4",
+          "description": "Airdrop Infinity – Bringing you the best crypto airdrops, in 2025! Discover and claim top cryptocurrency airdrops from various blockchain projects.",
+          "sameAs": [
+            "https://x.com/airdropinfiniti",
+            "https://www.facebook.com/airdropinfinity",
+            "https://medium.com/@airdropinfinity"
+          ]
         }
         `}
         </script>
