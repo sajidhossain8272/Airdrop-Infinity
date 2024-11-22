f14e6506b825d92a39cac08def71469ba2349206
sajidhossain8272 <sajidhossain8272@gmail.com>
2024-11-22 19:06:44 +0600

    favicon updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,28 @@ import Footer from "./Components/Footer";
 import Nav from "./Components/Nav";
 import { HeadProvider } from "react-head";
 import { SpeedInsights } from "@vercel/speed-insights/react";
-
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
+      <Analytics />
       <Helmet>
-        <title>Airdrop Infinity | Bringing you the best crypto airdrops, in 2025!</title>
+        <title>
+          Airdrop Infinity | Bringing you the best crypto airdrops, in 2025!
+        </title>
 
         {/* Structured Data */}
-        <script type="application/ld+json">
+        <script type='application/ld+json'>
           {JSON.stringify({
             "@context": "https://schema.org",
             "@type": "Organization",
-            "name": "Airdrop Infinity",
-            "url": "https://www.airdropinfinity.com/",
-            "logo": "../public/Logo-t.png",
-            "description":
+            name: "Airdrop Infinity",
+            url: "https://www.airdropinfinity.com/",
+            logo: "../public/Logo-t.png",
+            description:
               "Airdrop Infinity – Bringing you the best crypto airdrops, in 2025! Discover and claim top cryptocurrency airdrops from various blockchain projects.",
-            "sameAs": [
+            sameAs: [
               "https://x.com/airdropinfiniti",
               "https://www.facebook.com/airdropinfinity",
               "https://medium.com/@airdropinfinity",
@@ -32,7 +35,10 @@ function App() {
         </script>
 
         {/* Google Analytics */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-4KTTR97QHT"></script>
+        <script
+          async
+          src='https://www.googletagmanager.com/gtag/js?id=G-4KTTR97QHT'
+        ></script>
         <script>
           {`
             window.dataLayer = window.dataLayer || [];
